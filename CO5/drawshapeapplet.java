efa1b71813e6725826514e9a82e3c370d551970d
anjukc <94047190+anjukc@users.noreply.github.com>
2022-06-09 16:09:45 +0530

    8/6/2022

---
+++ CO5/drawshapeapplet.java
@@ -0,0 +1,10 @@
+package anju;
+
+import java.awt.*;
+import java.applet.*;
+
+public class draw_shape extends Applet{
+public void paint(Graphics g){
+g.setColor(Color.magenta);
+g.drawLine(20,30,20,300);
+g.drawRect(70,100,30,30);}}
